e05392d2ca6b5bd58a9357fa778644ceee1f7e6d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 09:14:18 -0300

    Adicionando API  e gráfico na Dashboard

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import {useNavigate} from "react-router-dom"
+import {Chart} from 'react-google-charts'
+import data from '../teste.json'
+
+//simulando os dados do banco
+
+  export const options={
+    isStacked:true,
+    height:300,
+    legend:{ position:"top", maxLines:3},
+    vAxis:{minValue:0}
+};
 
 const Dashboard = () => {
+  const navigate=useNavigate();
+
+//  FUNÇÃO handleLogout
+const handleLogout=()=>{
+  localStorage.removeItem("token");
+  navigate("/")
+}
+
+  
+
+
   return (
-    <div>Seja Bem Vindo ao Dashboard</div>
+    <div>
+      <h1>Seja Bem-vindo ao Dashboard</h1>
+      <button onClick={handleLogout} className=" mt-6 w-[100px] h-[60px] bg-blue-950 text-white rounded-2xl">
+        Logout
+      </button>
+
+      <Chart
+      chartType="AreaChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+
+      />
+
+
+
+
+
+
+    </div>
   )
 }
 
